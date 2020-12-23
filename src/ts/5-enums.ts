2a845afc28a9e2927bf939e8ab9947334347605a
Nicholas Newman <nicholas.newman1@hotmail.com>
2020-12-23 14:02:32 -0500

    finished enums

---
+++ src/ts/5-enums.ts
@@ -0,0 +1,13 @@
+/* Enum: Having some readable constants that you want represented as a number
+(ex: days of the week, compass directions, etc). This type is added by 
+TypeScript and does not exist in vanilla JS. Enum is a custom type and by
+convention the first letter is capitalized. It is also a convention to
+uppercase the values in an enum. */
+enum Directions {
+  NORTH,
+  SOUTH,
+  EAST,
+  WEST,
+}
+
+//console.log(Directions.EAST);
