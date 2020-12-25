e5c98b6c0b697a88a1e0af090f60aca6c38663ad
Nicholas Newman <nicholas.newman1@hotmail.com>
2020-12-25 08:29:24 -0500

    added type casting to dom elements

--- src/ts/13-dom-elements.ts
+++ src/ts/13-dom-elements.ts
@@ -8,8 +8,17 @@ if (button) {
   button.addEventListener('click', () => console.log('click'));
 }
 
+// The above if check can be simplified using a ?
+
+button?.addEventListener('click', () => console.log('click'));
+
 /* if you know for sure an element exists, you could put a ! after selecting
 it, or a ? before calling a method on the variable. */
 
-// Ex: const button = document.querySelector('button')!;
-// Or Ex: button?.addEventListener('click', () => console.log('click'));
+const button2 = document.querySelector('button')!;
+
+/* When selecting an element by class name, typescript won't know what type of
+element is being selected. This is when type casting comes in handy. Type
+casting specifies the type of html element and eliminates the need for ! */
+
+const button3 = document.querySelector('.button') as HTMLButtonElement;
